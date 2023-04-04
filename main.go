0a51577279c1f7d87279c7f84854de57ec98ff53
mynameisjonatron <mynameisjonatron>
2023-04-04 11:52:42 +0100

    hardware add

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
-import (
-	"onescan/scans"
-	"strconv"
-	"time"
-)
+import "onescan/scans"
 
 func main() {
-	s := new(scans.SynScan)
+	/*s := new(scans.SynScan)
 	ports := make([]string, 100)
 	for i := 0; i < len(ports); i++ {
 		ports[i] = strconv.Itoa(i)
 	}
-	s.Start(ports, "192.168.1.1", time.Second)
+	s.Start(ports, "192.168.1.1", time.Second)*/
+	s := new(scans.ArpScan)
+	s.Start()
 }
--- scans/arpscan.go
+++ scans/arpscan.go
@@ -1,6 +1,7 @@
 package scans
 
 import (
+	"fmt"
 	"log"
 	"net"
 )
@@ -8,8 +9,8 @@ import (
 type ArpScan struct {
 }
 
-func Start() {
-
+func (scan *ArpScan) Start() {
+	localMAC()
 }
 
 func arpPacket() {
@@ -17,10 +18,12 @@ func arpPacket() {
 }
 
 func localMAC() *[]net.Interface {
+	// Should find a way to know which non-loopback interface we want
 	intface, err := net.Interfaces()
 	if err != nil {
 		log.Printf("Cannot get interfaces %s", err)
 		return nil
 	}
+	fmt.Println(intface)
 	return &intface
 }

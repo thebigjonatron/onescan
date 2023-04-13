c969cd93be514178228b4d9ec243c46e310d73ae
vanderpluijmg <vanderpluijmgregory@gmail.com>
2023-04-13 14:19:32 +0100

    default params

--- controller/controller.go
+++ controller/controller.go
@@ -34,6 +34,19 @@ func findMode(arg string) scans.Scanner {
 	}
 }
 
+// Define if it's a range or not
 func addressResolver(arg string) {
 
+}
+
+func defaults() scans.Utils {
+	ports := make([]string, 100)
+	myUtils := scans.Utils{
+		Ip:      "192.168.0.1",
+		Mask:    "255.255.255.0",
+		Intface: "eth0",
+		Ssid:    "MyWifiNetwork",
+		Ports:   ports,
+	}
+	return myUtils
 }
